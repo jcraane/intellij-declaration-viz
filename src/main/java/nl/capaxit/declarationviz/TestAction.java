9ee01e7b4f7ff08e96b2b9c2c3a62747b3ae7674
jcraane <jcraane@gmail.com>
2017-03-22 16:35:41 +0100

    Add x ofset to element position (there must be a better way to calculate this offset).

--- src/main/java/nl/capaxit/declarationviz/TestAction.java
+++ src/main/java/nl/capaxit/declarationviz/TestAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
@@ -26,27 +27,27 @@ public class TestAction extends AnAction {
     public void actionPerformed(final AnActionEvent e) {
         final Project project = e.getData(CommonDataKeys.PROJECT);
         final Editor editor = e.getData(CommonDataKeys.EDITOR);
-//        e.getPresentation().setVisible(project != null && editor != null);
-        final Caret currentCaret = editor.getCaretModel().getCurrentCaret();
+        final CaretModel caretModel = editor.getCaretModel();
+        final Caret currentCaret = caretModel.getCurrentCaret();
         final Document document = editor.getDocument();
         final Point point = editor.logicalPositionToXY(currentCaret.getLogicalPosition());
-        System.out.println("point = " + point);
         final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
-        final PsiElement elementAtCaret = psiFile.findElementAt(editor.getCaretModel().getOffset());
-        System.out.println("elementAtCaret = " + elementAtCaret);
+
+        final PsiElement elementAtCaret = psiFile.findElementAt(caretModel.getOffset());
 
 //        1. first we find the parent PsiReferenceExpression
         final PsiReferenceExpression parent = PsiTreeUtil.getParentOfType(elementAtCaret, PsiReferenceExpression.class);
         if (parent != null) {
-            int i = 0;
             final PsiElement declaration = parent.resolve();
             final Point declarationPosition = editor.visualPositionToXY(editor.offsetToVisualPosition(declaration.getTextOffset()));
+//            final Point declarationPosition = editor.visualPositionToXY(editor.offsetToVisualPosition(declaration.getChildren()[6].getTextOffset()));
             final Graphics graphics = editor.getComponent().getGraphics();
             graphics.setColor(new Color(131, 142, 255, 128));
             ((Graphics2D) graphics).setStroke(new BasicStroke(3));
             final int xOffset = 25;
+            final int elementXOffSet = 50;
             final int yOffset = 10;
-            graphics.drawLine(point.x + xOffset, point.y + yOffset, declarationPosition.x, declarationPosition.y);
+            graphics.drawLine(point.x + xOffset, point.y + yOffset, declarationPosition.x + elementXOffSet, declarationPosition.y);
         }
     }
 }
