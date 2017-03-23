8fddbed1841efa3da58ce66202b283052d25f074
jcraane <jcraane@gmail.com>
2017-03-23 20:09:55 +0100

    Refactoring and cleanup code.

--- src/main/java/nl/capaxit/idea/plugins/usagevisualizer/UsageVisualizerAction.java
+++ src/main/java/nl/capaxit/idea/plugins/usagevisualizer/UsageVisualizerAction.java
@@ -3,11 +3,9 @@ package nl.capaxit.idea.plugins.usagevisualizer;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.impl.EditorImpl;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
@@ -16,64 +14,85 @@ import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiReferenceExpression;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Simple action for testing various Idea plugin parts. Action should be invoked when hovering over
  * an element in the editor, perhaps by holding the control button.
- *
+ * <p>
  * Created by jamiecraane on 25/10/2016.
  */
 public class UsageVisualizerAction extends AnAction {
+    // todo there must be a better way to determine this offset.
+    public static final int FIXED_X_OFFSET = 100;
+
     @Override
     public void actionPerformed(final AnActionEvent e) {
+        doWork(e);
+    }
+
+    private void doWork(final AnActionEvent e) {
         final Project project = e.getData(CommonDataKeys.PROJECT);
         final Editor editor = e.getData(CommonDataKeys.EDITOR);
-        final CaretModel caretModel = editor.getCaretModel();
-        final Caret currentCaret = caretModel.getCurrentCaret();
-        final Document document = editor.getDocument();
-        final Point point = editor.logicalPositionToXY(currentCaret.getLogicalPosition());
-        final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+        if (project == null || editor == null) {
+            return;
+        }
 
-        final PsiElement elementAtCaret = psiFile.findElementAt(caretModel.getOffset());
+        final PsiElement elementAtCaret = findElementAtCaret(project, editor);
+        if (elementAtCaret == null) {
+            return;
+        }
 
         final PsiReferenceExpression parent = PsiTreeUtil.getParentOfType(elementAtCaret, PsiReferenceExpression.class);
         if (parent != null) {
-            final java.util.List<LineSpec> lines = new ArrayList<>(5);
-
-            final int verticalScrollOffset = editor.getScrollingModel().getVerticalScrollOffset();
-            final PsiElement declaration = parent.resolve();
-
-            final int elementXOffset = 100;
-            final int caretXOffset = 75;
-            final int yOffset = 15;
-            final Point caretPosition = new Point(point.x + caretXOffset, point.y + yOffset);
-            final Point declarationPosition = editor.visualPositionToXY(editor.offsetToVisualPosition(declaration.getTextOffset()));
-            final Point declarationPoint = new Point(declarationPosition.x + elementXOffset, declarationPosition.y - verticalScrollOffset);
-            final Collection<PsiReference> references = ReferencesSearch.search(declaration).findAll();
-            references.forEach(reference -> {
-                final PsiElement element = reference.getElement();
-                final Point elementPosition = editor.visualPositionToXY(editor.offsetToVisualPosition(element.getTextOffset()));
-                lines.add(LineSpec.create(declarationPoint, new Point(elementPosition.x + elementXOffset, elementPosition.y - verticalScrollOffset)));
-            });
-
-            final Graphics graphics = editor.getComponent().getGraphics();
-            graphics.setColor(new Color(131, 142, 255, 128));
-            ((Graphics2D) graphics).setStroke(new BasicStroke(2));
-            final int logicalPositionToOffset = editor.logicalPositionToOffset(((EditorImpl) editor).offsetToLogicalPosition(declaration.getTextOffset()));
-            // todo there must be a better way to determine these offsets.
-
-//            lines.add(LineSpec.create(caretPosition, declarationPoint));
-
-            drawLines(lines, graphics);
+            findAndDrawUsagesLines(editor, parent);
         }
     }
 
-    private void drawLines(final List<LineSpec> lines, final Graphics graphics) {
+    private void findAndDrawUsagesLines(final Editor editor, final PsiReferenceExpression parent) {
+        final int verticalScrollOffset = editor.getScrollingModel().getVerticalScrollOffset();
+        final int elementXOffset = FIXED_X_OFFSET;
+
+        final PsiElement declaration = parent.resolve();
+
+        final Point declarationPosition = editor.visualPositionToXY(editor.offsetToVisualPosition(declaration.getTextOffset()));
+        final Point declarationPoint = new Point(declarationPosition.x + elementXOffset, declarationPosition.y - verticalScrollOffset);
+        final Collection<PsiReference> references = ReferencesSearch.search(declaration).findAll();
+        final List<LineSpec> lines = references.stream()
+                .map(reference -> createUsageLineSpec(editor, verticalScrollOffset, elementXOffset, declarationPoint, reference))
+                .collect(Collectors.toList());
+
+        drawLines(lines, editor);
+    }
+
+    @NotNull
+    private LineSpec createUsageLineSpec(final Editor editor, final int verticalScrollOffset, final int elementXOffset, final Point declarationPoint, final PsiReference reference) {
+        final PsiElement element = reference.getElement();
+        final Point elementPosition = editor.visualPositionToXY(editor.offsetToVisualPosition(element.getTextOffset()));
+        return LineSpec.create(declarationPoint, new Point(elementPosition.x + elementXOffset, elementPosition.y - verticalScrollOffset));
+    }
+
+    private PsiElement findElementAtCaret(final Project project, final Editor editor) {
+        final CaretModel caretModel = editor.getCaretModel();
+        final Document document = editor.getDocument();
+        final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+        return psiFile.findElementAt(caretModel.getOffset());
+    }
+
+    private void drawLines(final List<LineSpec> lines, final Editor editor) {
+        final Graphics graphics = editor.getComponent().getGraphics();
+        graphics.setColor(new Color(131, 142, 255, 128));
+        ((Graphics2D) graphics).setStroke(new BasicStroke(2));
         lines.forEach(line -> graphics.drawLine(line.getStart().x, line.getStart().y, line.getEnd().x, line.getEnd().y));
+    }
+
+    @Override
+    public void update(final AnActionEvent e) {
+        doWork(e);
     }
 }
