cc5473f49f0b4101f115aff20d4150f6b6658137
jcraane <jcraane@gmail.com>
2017-04-06 23:57:05 +0200

    Add line type combo and color picker.

--- src/main/java/nl/capaxit/idea/plugins/usagevisualizer/configuration/UsageVisualizationConfiguration.java
+++ src/main/java/nl/capaxit/idea/plugins/usagevisualizer/configuration/UsageVisualizationConfiguration.java
@@ -2,11 +2,12 @@ package nl.capaxit.idea.plugins.usagevisualizer.configuration;
 
 import com.intellij.openapi.options.BaseConfigurable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.ui.components.panels.HorizontalBox;
+import com.intellij.ui.components.panels.VerticalBox;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.awt.*;
 
 /**
  * Created by jamiecraane on 06/04/2017.
@@ -28,8 +29,18 @@ public class UsageVisualizationConfiguration extends BaseConfigurable {
     @Override
     public JComponent createComponent() {
         final JPanel jPanel = new JPanel();
-        jPanel.add(new TextField());
-        jPanel.add(new TextField());
+        final VerticalBox root = new VerticalBox();
+        final HorizontalBox lineTypeHolder = new HorizontalBox();
+        lineTypeHolder.add(new JLabel("Line type"));
+        lineTypeHolder.add(new JComboBox<>(new String[]{"Straight line", "Bezier curve"}));
+        root.add(lineTypeHolder);
+
+        final HorizontalBox colorHolder = new HorizontalBox();
+        colorHolder.add(new JLabel("Line color"));
+        colorHolder.add(new JColorChooser());
+        root.add(colorHolder);
+
+        jPanel.add(root);
         return jPanel;
     }
 
