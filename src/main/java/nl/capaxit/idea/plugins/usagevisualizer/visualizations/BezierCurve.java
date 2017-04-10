d4e436a2a1aa42341cc3daf46445f81893d383a9
jcraane <jcraane@gmail.com>
2017-04-10 16:33:19 +0200

    Fixed endY of bezier curve.

--- src/main/java/nl/capaxit/idea/plugins/usagevisualizer/visualizations/BezierCurve.java
+++ src/main/java/nl/capaxit/idea/plugins/usagevisualizer/visualizations/BezierCurve.java
@@ -1,11 +1,11 @@
-package nl.capaxit.idea.plugins.usagevisualizer;
+package nl.capaxit.idea.plugins.usagevisualizer.visualizations;
 
 import java.awt.*;
 import java.awt.geom.QuadCurve2D;
 
 /**
- * todo provide a better endX position so that the curve points more towards the element instead of a fixed offset.
- *
+ * todo provide a better endX position so that the curve points more towards the beginning of the line instead of a fixed offset.
+ * <p>
  * Created by jamiecraane on 06/04/2017.
  */
 public class BezierCurve implements UsageVisualization {
@@ -16,7 +16,7 @@ public class BezierCurve implements UsageVisualization {
 
     /**
      * @param start start of the line. The bezier curve must find the y pos based on this point.
-     * @param end end of the line. The bezier curve must find the y pos based on this point.
+     * @param end   end of the line. The bezier curve must find the y pos based on this point.
      */
     public BezierCurve(final Point start, final Point end) {
         this.start = start;
@@ -33,13 +33,14 @@ public class BezierCurve implements UsageVisualization {
         int endY = end.y;
         if (start.y < end.y) {
             startY += Y_OFFSET;
+            endY += Y_OFFSET;
         } else {
             endY += Y_OFFSET;
         }
 
         int ctrlY;
         if (startY < endY) {
-             ctrlY = startY + Math.abs(endY - startY) / 2;
+            ctrlY = startY + Math.abs(endY - startY) / 2;
         } else {
             ctrlY = endY + Math.abs(endY - startY) / 2;
         }
@@ -52,16 +53,16 @@ public class BezierCurve implements UsageVisualization {
 
     private void drawArrowTip(final Graphics2D graphics, final int endY, final int ctrlY) {
         final int dx = START_X - (START_X - X_OFFSET), dy = endY - ctrlY;
-        double D = Math.sqrt(dx*dx + dy*dy);
+        double D = Math.sqrt(dx * dx + dy * dy);
         double xm = D - 10, xn = xm, ym = 5, yn = -5, x;
-        double sin = dy/D, cos = dx/D;
+        double sin = dy / D, cos = dx / D;
 
-        x = xm*cos - ym*sin + START_X - X_OFFSET;
-        ym = xm*sin + ym*cos + ctrlY;
+        x = xm * cos - ym * sin + START_X - X_OFFSET;
+        ym = xm * sin + ym * cos + ctrlY;
         xm = x;
 
-        x = xn*cos - yn*sin + START_X - X_OFFSET;
-        yn = xn*sin + yn*cos + ctrlY;
+        x = xn * cos - yn * sin + START_X - X_OFFSET;
+        yn = xn * sin + yn * cos + ctrlY;
         xn = x;
 
         final int[] xpoints = {START_X, (int) xm, (int) xn};
